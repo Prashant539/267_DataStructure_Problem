5615b8719ba73d663f355a1332b09834f03f642f
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-03-27 20:33:53 +0530

    [Prashant]Add: Performing custom linked list operation

---
+++ LinkedListProblem/LinkedListProblem/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to DSA Problem");
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(30);
+            linkedList.Add(50);
+            linkedList.Add(70);
+            linkedList.Display();
+            linkedList.RemoveFirstNode();
+            linkedList.Display();
+            linkedList.RemoveLastNode();
+            linkedList.Display();
+
+        }
+    }
+}
