a8a8f1429bf7aa045acef110bc82959f5217d8a2
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-03-27 21:07:25 +0530

    [Prashant]Add: Performing custom stack operations

---
+++ LinkedListProblem/LinkedListProblem/CustomStack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+    public class CustomStack
+    {
+        public Node top;
+        public CustomStack()
+        {
+            this.top = null;
+
+        }
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top != null)
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to Stack", value);
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        internal void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of stack", this.top.data);
+        }
+        internal void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("stack is empty, Deletion is not possible");
+                return;
+            }
+            Console.WriteLine("value popped is {0}", this.top.data);
+            this.top = this.top.next;
+        }
+    }
+}
--- LinkedListProblem/LinkedListProblem/Program.cs
+++ LinkedListProblem/LinkedListProblem/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace LinkedListProblem
@@ -8,16 +9,36 @@ namespace LinkedListProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to DSA Problem");
-            LinkedList linkedList = new LinkedList();
-            linkedList.Add(30);
-            linkedList.Add(50);
-            linkedList.Add(70);
-            linkedList.Display();
-            linkedList.RemoveFirstNode();
-            linkedList.Display();
-            linkedList.RemoveLastNode();
-            linkedList.Display();
+            Console.WriteLine("Enter the Options to perform: ");
+            Console.WriteLine("1:Custom Linked List\n2:custom Stack");
+            int options = Convert.ToInt32(Console.ReadLine());
+            switch (options)
+            {
+                case 1:
+                    LinkedList linkedList = new LinkedList();
+                    linkedList.Add(30);
+                    linkedList.Add(50);
+                    linkedList.Add(70);
+                    linkedList.Display();
+                    linkedList.RemoveFirstNode();
+                    linkedList.Display();
+                    linkedList.RemoveLastNode();
+                    linkedList.Display();
+                    break;
+                 case 2:
+                    CustomStack customStack = new CustomStack();
+                    customStack.Push(70);
+                    customStack.Push(30);
+                    customStack.Push(56);
+                   // customStack.IsEmpty();
+                    customStack.Pop();
+                    customStack.Peek();
+                    customStack.Display();
+                    break;
 
+            }
+    
         }
+   
     }
 }
